238f1a81575972b8c4925c28111d08dc9f3c515d
eric <ericgip97@gmail.com>
2020-09-07 21:00:50 -0700

    made homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
-import VideoUpload from "../src/components/views/VideoUpload"
-
+import VideoUpload from "./components/views/VideoUpload/VideoUpload";
+import HomePage from "./components/views/HomePage/HomePage";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -10,9 +10,18 @@ class App extends Component {
     return (
       <div className="App">
         <BrowserRouter>
-          Hello
+          <header>Hello - Todo: make header / navbar</header>
+          <br></br>
+          <Link to="/"> Home </Link>
+          <br></br>
+          <Link to="/HomePage"> Videos </Link>
+          <br></br>
+          <Link to="/VideoUpload"> Video upload page </Link>
           <Route path="/VideoUpload" component={VideoUpload}></Route>
+          <Route path="/HomePage" component={HomePage}></Route>
         </BrowserRouter>
+        <br></br>
+        Hello - Todo: make footer
       </div>
     );
   }
---
+++ client/src/components/views/HomePage/HomePage.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Card from '@material-ui/core/Card';
+import DialogTitle from "@material-ui/core/DialogTitle"
+import Avatar from '@material-ui/core/Avatar';
+
+const { MetaInfo } = Card;
+
+function HomePage() {
+    return (
+        <div style={{ width: "85%", margin: "3rem auto"}}>
+            <DialogTitle> Recommended </DialogTitle>
+            <hr />
+
+            <div>
+                <div style={{ position: "relative" }}>
+                    <img style={{width: "100%"}} src="" alt="test" />
+                    <div>
+                        <span> minute : second </span>
+                    </div>
+                </div>
+                <Card title="">
+                    <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                </Card>
+                <span> writer name </span>
+                <span style={{marginLeft:'3rem'}}> Views Count </span>
+                    <span> date </span>
+                </div>
+        </div>
+    )
+}
+
+export default HomePage
